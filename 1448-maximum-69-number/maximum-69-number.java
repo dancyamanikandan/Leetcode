f0dab031b30693c1345f7fefa5f3c8333247f59b
dancyamanikandan <danciya31204@gmail.com>
2025-08-16 22:37:50 +0530

    Time: 2 ms (17.88%) | Memory: 40.4 MB (86.42%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,8 +1,8 @@
 class Solution {
     public int maximum69Number (int num) {
         String s=String.valueOf(num);
-        String ans=s.replaceFirst("6","9");
-        int max=Integer.parseInt(ans);
+        s=s.replaceFirst("6","9");
+        int max=Integer.parseInt(s);
         return max;
         
     }
