c34c01256a0f9dc8c42a6c2b16e681f1d8273b37
dancyamanikandan <danciya31204@gmail.com>
2025-08-25 12:17:35 +0530

    Time: 2 ms (58.89%) | Memory: 42.4 MB (93.97%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,10 +1,20 @@
 import java.math.BigInteger;
 class Solution {
     public String addStrings(String num1, String num2) {
-        BigInteger a=new BigInteger(num1);
-        BigInteger b=new BigInteger(num2);
-        BigInteger c=a.add(b);
-        return c.toString();
-        
+       int i=num1.length()-1;
+       int j=num2.length()-1;
+       int carry=0;
+       StringBuilder sb=new StringBuilder();
+       while(i>=0 || j>=0 || carry!=0){
+        int digit1=(i>=0)?num1.charAt(i)-'0':0;//last digit
+        int digit2=(j>=0)?num2.charAt(j)-'0':0;
+        int sum=digit1+digit2+carry;
+        sb.append(sum%10);
+        carry=sum/10;
+        i--;
+        j--;
+       }
+       return sb.reverse().toString();
+    
     }
 }
